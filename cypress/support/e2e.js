5e7dd042bbdd781402b583b44d2d5dc1a1e2154b
SHENGXING LU <slu@SHENGXINGs-MacBook-Air.local>
2025-11-04 16:36:16 +0100

    increase timeout

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,6 +2,6 @@
 require('./commands')
 
 // Custom command to select elements by data-cy attribute
-Cypress.Commands.add('getBySel', (selector) => {
-  return cy.get(`[data-cy="${selector}"]`)
+Cypress.Commands.add('getBySel', (selector, options) => {
+  return cy.get(`[data-cy="${selector}"]`, options)
 })
